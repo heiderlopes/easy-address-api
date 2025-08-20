4684f892454059f0422aa5604384c21170bf5cea
Heider Lopes <heidertreinamentos@gmail.com>
2025-08-20 09:38:37 -0300

    Ajuste no campo cidade

--- src/models/address.model.js
+++ src/models/address.model.js
@@ -2,15 +2,15 @@ let addresses = [];
 let idCounter = 1;
 
 class Address {
-  constructor({ nomeUsuario, cep, logradouro, bairro, localidade, uf, tipo }) {
+  constructor({ userName, cep, logradouro, bairro, cidade, uf, tipo }) {
     this.id = idCounter++;
-    this.nomeUsuario = nomeUsuario;
+    this.userName = userName;
     this.cep = cep;
     this.logradouro = logradouro;
     this.bairro = bairro;
-    this.cidade = cidade;
+    this.cidade = cidade; // mudado de localidade
     this.uf = uf;
-    this.tipo = tipo; // Ex: residencial, comercial
+    this.tipo = tipo;
   }
 }
 
